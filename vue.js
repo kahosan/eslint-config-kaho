8bb6447a56590df03eff37d87141316374ba5e91
kahosan <15116355657l@gmail.com>
2022-09-20 09:54:49 +0800

    chore: update

--- vue.js
+++ vue.js
@@ -1,3 +1,9 @@
 module.exports = {
   extends: ['@antfu/eslint-config-vue'],
+  rules: {
+    '@typescript-eslint/semi': ['error', 'always'],
+    'curly': ['error', 'multi-line'],
+    '@typescript-eslint/brace-style': ['error', '1tbs', { allowSingleLine: true }],
+    '@typescript-eslint/indent': ['error', 2, { SwitchCase: 1 }],
+  },
 };
