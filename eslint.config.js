2096546d1151d198e410b11b4537e5c00655df77
kahosan <15116355657l@gmail.com>
2024-05-06 12:26:25 +0800

    chore: update deps

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,4 @@
-// eslint-disable-next-line import/no-unresolved -- local
+// eslint-disable-next-line import-x/no-unresolved -- local
 import { kaho } from 'eslint-config-kaho';
 
 export default kaho({ ts: { tsconfigPath: './tsconfig.json' } });
