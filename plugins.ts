8a2a85a2047de0b1180d2c4e05e7c1f1f1137c9f
kahosan <15116355657l@gmail.com>
2025-11-27 12:24:45 +0800

    chore: update import sort rules

--- plugins.ts
+++ plugins.ts
@@ -13,21 +13,34 @@ export const sorting: FlatESLintConfigItem = {
   rules: {
     'perfectionist/sort-exports': ['error', { order: 'asc', type: 'natural' }],
     'perfectionist/sort-imports': ['error', {
+      /** @see {@link https://perfectionist.dev/rules/sort-imports#groups} */
       groups: [
-        'type',
-        ['parent-type', 'sibling-type', 'index-type', 'internal-type'],
-
-        'builtin',
-        'external',
-        'internal',
-        ['parent', 'sibling', 'index'],
+        'type-builtin',
+        'type-import',
+        'type-internal',
+        ['type-parent', 'type-sibling', 'type-index'],
+        'default-value-builtin',
+        'named-value-builtin',
+        'value-builtin',
+        'default-value-external',
+        'named-value-external',
+        'value-external',
+        'default-value-internal',
+        'named-value-internal',
+        'value-internal',
+        ['default-value-parent', 'default-value-sibling', 'default-value-index'],
+        ['named-value-parent', 'named-value-sibling', 'named-value-index'],
+        ['wildcard-value-parent', 'wildcard-value-sibling', 'wildcard-value-index'],
+        ['value-parent', 'value-sibling', 'value-index'],
         'side-effect',
-        'object',
+        'style',
         'unknown'
       ],
-      newlinesBetween: 'ignore',
+      newlinesBetween: 'always',
+      partitionByComment: true,
+      type: 'line-length',
       order: 'asc',
-      type: 'natural'
+      fallbackSort: { type: 'alphabetical', order: 'asc' }
     }],
     'perfectionist/sort-named-exports': ['error', { order: 'asc', type: 'natural' }],
     'perfectionist/sort-named-imports': ['error', { order: 'asc', type: 'natural' }]
